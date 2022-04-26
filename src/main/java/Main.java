87e2891acfd26bb46c7eae2ad3aecee9cb195287
BezBzz <apaser90@mail.ru>
2022-04-26 23:12:36 +0300

    Fix subscription

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,17 +8,17 @@ public class Main {
 
         post.birthday.day = 13;
         post.birthday.month = 6;
-        post.birthday.year =1999;
+        post.birthday.year = 1999;
         post.name = "Иван";
         post.passport = "4444 № 44444444";
         post.patronymic = "Иванович";
         post.phone = "+7 (999)-999-99-99";
         post.surname = "Иванов";
-        post.subscription = "True";
-
-            }
+        post.subscription = true;
 
     }
+
+}
 
 
 
